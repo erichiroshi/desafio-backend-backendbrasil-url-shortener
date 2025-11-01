dd0c6017aa02c76031d81ac296e3823efc356119
Eric Hiroshi <erichiroshi@hotmail.com>
2025-10-31 21:58:13 -0400

    controller redirect

--- src/main/java/com/erichiroshi/urlshortener/controller/UrlController.java
+++ src/main/java/com/erichiroshi/urlshortener/controller/UrlController.java
@@ -1,9 +1,15 @@
 package com.erichiroshi.urlshortener.controller;
 
+import java.net.URI;
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 import org.apache.commons.lang3.RandomStringUtils;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -37,6 +43,21 @@ public class UrlController {
         String redirectUrl = servletRequest.getRequestURL().toString().replace("shorten-url", id);
 
         return ResponseEntity.ok(new ShortenUrlResponse(redirectUrl));
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<Void> redirect(@PathVariable String id) {
+
+        Optional<UrlEntity> url = urlRepository.findById(id);
+
+        if (url.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setLocation(URI.create(url.get().getFullUrl()));
+
+        return ResponseEntity.status(HttpStatus.FOUND).headers(headers).build();
     }
 
 }
